b0974c188aedbd1cdb99bf08e17642dbd3438c28
YoussefNassef <lionmssei@yahoo.com>
2023-11-09 14:04:55 +0200

    add server sitting

--- server.js
+++ server.js
@@ -5,13 +5,34 @@ const app = require('./app');
 const DB = process.env.DATABASE;
 const port = process.env.PORT;
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 mongoose
   .connect(DB)
   .then(() => console.log('DB connection Successfully'))
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app is listen in port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
 });
